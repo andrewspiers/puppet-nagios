b0e4d560c42b558b8412d07d406991438cc37694
Andrew Spiers <andrew@andrewspiers.net>
2014-11-27 18:35:00 +1100

    initial move

---
+++ manifests/nrpe/command.pp
@@ -0,0 +1,22 @@
+define nagios::nrpe::command ($check_command) {
+
+  File <| tag == 'nrpe' |>
+  Package <| tag == 'nrpe' |>
+  Service <| tag == 'nrpe' |>
+
+  $nrpe = $osfamily ? {
+    RedHat  =>  'nrpe',
+    Debian  =>  'nagios-nrpe-server',
+    default =>  'nagios-nrpe-server',
+  }
+
+  file { "/etc/nagios/nrpe.d/${name}.cfg":
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => [ Package[ $nrpe ], File['/etc/nagios/nrpe.d'], ],
+    notify  => Service[ $nrpe ],
+    content => template('nagios/nrpe_command.erb');
+  }
+
+}
