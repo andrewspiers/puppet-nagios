d65651f420ba357134cd4804d6bf63f573e98da5
Andrew Spiers <andrew@andrewspiers.net>
2014-11-27 18:16:14 +1100

    nagios::nrpe::command: use $nrpe var for package & service to notify.

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -102,12 +102,18 @@ define nagios::nrpe::command ($check_command) {
   Package <| tag == 'nrpe' |>
   Service <| tag == 'nrpe' |>
 
+  $nrpe = $osfamily ? {
+    RedHat  =>  'nrpe',
+    Debian  =>  'nagios-nrpe-server',
+    default =>  'nagios-nrpe-server',
+  }
+
   file { "/etc/nagios/nrpe.d/${name}.cfg":
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-    require => [ Package['nagios-nrpe-server'], File['/etc/nagios/nrpe.d'], ],
-    notify  => Service['nagios-nrpe-server'],
+    require => [ Package[ $nrpe ], File['/etc/nagios/nrpe.d'], ],
+    notify  => Service[ $nrpe ],
     content => template('nagios/nrpe_command.erb');
   }
 
